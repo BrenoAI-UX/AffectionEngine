04178cfc3ce7136e92f916e26dd318c1befe019f
BrenoAI-UX <breno.teixeiras777@gmail.com>
2025-07-24 03:43:59 -0300

    Create 3EmpathyResponder.js

---
+++ src/core/3EmpathyResponder.js
@@ -0,0 +1,5 @@
+export function respondWithEmpathy(input) {
+  if (input.includes("triste")) return "Sinto muito... estou aqui com você 💙";
+  if (input.includes("feliz")) return "Que alegria! Adoro ver você sorrindo 😊";
+  return "Estou ouvindo com carinho 💛";
+}
